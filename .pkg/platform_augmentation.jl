f003d76fc6fe0a01d81a128c4ce828afb75312f6
jlbuild <juliabuildbot@gmail.com>
2026-05-15 18:36:46 +0000

    Qt6Tools_jll build 6.8.2+1

--- .pkg/platform_augmentation.jl
+++ .pkg/platform_augmentation.jl
@@ -1,11 +1,13 @@
 using Base.BinaryPlatforms
+using Libdl: dlopen, dlsym
+
 function augment_llvm!(platform::Platform)
     haskey(platform, "llvm_version") && return platform
 
     llvm_version = Base.libllvm_version
     # does our LLVM build use assertions?
     llvm_assertions = try
-        cglobal((:_ZN4llvm24DisableABIBreakingChecksE, Base.libllvm_path()), Cvoid)
+        dlsym(dlopen(Base.libllvm_path()), "_ZN4llvm24DisableABIBreakingChecksE")
         false
     catch
         true
@@ -18,6 +20,4 @@ function augment_llvm!(platform::Platform)
     return platform
 end
 
-function augment_platform!(platform::Platform)
-    augment_llvm!(platform)
-end
+augment_platform!(platform::Platform) = augment_llvm!(platform)
--- src/Qt6Tools_jll.jl
+++ src/Qt6Tools_jll.jl
@@ -7,5 +7,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Qt6Tools")
-JLLWrappers.@generate_main_file("Qt6Tools", UUID("41098d5d-486a-5be3-bd44-b646bda58606"))
+JLLWrappers.@generate_main_file("Qt6Tools", Base.UUID("41098d5d-486a-5be3-bd44-b646bda58606"))
 end  # module Qt6Tools_jll
